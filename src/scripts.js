ea5f43546706b26a434b45dc7ac6c4116911aedc
joaobraga <joaosb004@gmail.com>
2023-09-21 09:35:00 -0300

    code formatted

--- src/scripts.js
+++ src/scripts.js
@@ -3,19 +3,18 @@ const app = {
         return {
             tasks: [],
             task: "",
-        }
+        };
     },
     methods: {
         addTask() {
-            if(this.task !== "")
-                this.tasks.push({task: this.task, active: false});
+            if (this.task !== "") this.tasks.push({ task: this.task, active: false });
 
             this.task = "";
         },
         removeAll() {
             this.tasks = [];
-        }
-    }
+        },
+    },
 };
 
-Vue.createApp(app).mount("#app")
+Vue.createApp(app).mount("#app");
