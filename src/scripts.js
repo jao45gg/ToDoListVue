cadf7c0aac27afbb98aaf62bf483276426309817
joaobraga <joaosb004@gmail.com>
2023-09-21 09:31:55 -0300

    Project done

---
+++ src/scripts.js
@@ -0,0 +1,21 @@
+const app = {
+    data() {
+        return {
+            tasks: [],
+            task: "",
+        }
+    },
+    methods: {
+        addTask() {
+            if(this.task !== "")
+                this.tasks.push({task: this.task, active: false});
+
+            this.task = "";
+        },
+        removeAll() {
+            this.tasks = [];
+        }
+    }
+};
+
+Vue.createApp(app).mount("#app")
